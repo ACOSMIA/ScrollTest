355a834e3533edd6a6172e71f57ac471db71a714
ACOSMIA <13533284774@163.com>
2023-08-28 19:06:33 +0800

    planC_Fourth

---
+++ Assets/TestGit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("看到这个证明你在版本4");
+    }
+
+}
